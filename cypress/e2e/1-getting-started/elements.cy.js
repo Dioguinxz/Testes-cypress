bcd910ac74fdc66406d04f490f0a1d2968d88a90
Dioguinxz <doassis@hotmail.com.br>
2024-09-30 10:33:45 -0300

    testes para capturar alerts

--- cypress/e2e/1-getting-started/elements.cy.js
+++ cypress/e2e/1-getting-started/elements.cy.js
@@ -45,7 +45,7 @@ describe('Work with basic elements', () => {
 
     })
 
-    it.only('RadioButton', () => {
+    it('RadioButton', () => {
         cy.get('#formSexoFem')
             .click()
             .should('be.checked')
@@ -55,7 +55,7 @@ describe('Work with basic elements', () => {
             .should('have.length', 2)
     })
 
-    it.only('Checkbox', () => {
+    it('Checkbox', () => {
         cy.get('#formComidaPizza')
             .click()
             .should('be.checked')
@@ -66,13 +66,42 @@ describe('Work with basic elements', () => {
 
     })
 
-    it.only('Combo', () => {
+    it('Combo', () => {
         cy.get('[data-test=dataEscolaridade]')
             .select('2o grau completo')
             .should('have.value', '2graucomp')
+
+        cy.get('[data-test=dataEscolaridade]')
+            .select('1o grau completo')
+            .should('have.value', '1graucomp')
+
+        cy.get('[data-test=dataEscolaridade] option').should('have.length', 8)
+
+        cy.get('[data-test=dataEscolaridade] option')
+            .then($arr => {
+                const values = []
+                $arr.each(function () {
+                    values.push(this.innerHTML)
+                })
+                expect(values).to.include.members(["Superior", "Mestrado"])
+            })
+
     })
 
     it.only('MultipleCombo', () => {
-        cy.get('[data-testid=dataEsportes]').select(['natacao', 'Corrida', 'nada'])
+        cy.get('[data-testid=dataEsportes]')
+        .select(['natacao', 'Corrida', 'nada'])
+        cy.get('[data-testid=dataEsportes]').then($el => {
+           expect($el.val()).to.be.deep.equal(['natacao', 'Corrida', 'nada'])
+            expect($el.val()).to.have.length(3)
+        })
+
+        cy.get(('[data-testid=dataEsportes]'))
+        .invoke('val').should('eql', ['natacao', 'Corrida', 'nada'])
+        
+   
+   
+   
+   
     })
 })
