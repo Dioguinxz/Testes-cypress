12b42a822065a79d115dd976ef19fd105ca8a2ac
Dioguinxz <doassis@hotmail.com.br>
2024-09-30 13:34:36 -0300

    testes de iframe

--- cypress/e2e/1-getting-started/alert.cy.js
+++ cypress/e2e/1-getting-started/alert.cy.js
@@ -13,7 +13,6 @@ describe('Work with alerts', () => {
     it('Alert', () => {
         cy.get('#alert').click()
         cy.on('window:alert', (msg) => {
-            console.log(msg)
             expect(msg).to.be.equal('Alert Simples')
         })
     })
@@ -49,14 +48,40 @@ describe('Work with alerts', () => {
         cy.get('#confirm').click()
     })
 
-    it.only('Prompt', () => {
+    it('Prompt', () => {
         cy.window().then(win => {
-            cy.stub(win, 'prompt')
+            cy.stub(win, 'prompt').returns(42)
         })
-        // cy.on('window:prompt', msg => {
-        //     expect(msg).to.be.equal('Confirm Simples')
-        // })
-        // cy.get('#prompt').click()
+
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Era 42?')
+        })
+
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal(':D')
+        })
+
+        cy.get('#prompt').click()
+    })
+
+    it.only('Validando mensagens', () => {
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+
+        cy.get('#formNome').type('Wagner')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+
+        cy.get('[data-cy=dataSobrenome]').type('Aquino')
+        cy.get('#formCadastrar').click()
+            .then(() => expect(stub.getCall(2)).to.be.calledWith('Sexo eh obrigatorio'))
+
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+
+        cy.get('#resultado > :nth-child(1)').should('contain', 'Cadastrado!')
     })
 
 })
