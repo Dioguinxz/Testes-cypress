20a9e3a5d98b4bc79fc8d074f5f322c034f3d939
Dioguinxz <doassis@hotmail.com.br>
2024-10-02 08:12:34 -0300

    functional

--- cypress/e2e/barriga/functional.cy.js
+++ cypress/e2e/barriga/functional.cy.js
@@ -6,18 +6,29 @@ describe("Should test at a functional level", () => {
     cy.get('[data-test="email"]').type("a@a");
     cy.get('[data-test="passwd"]').type("a");
     cy.get(".btn").click();
-    cy.get(".toast-message").should("contain", 'Bem vindo');
+    cy.get(".toast-message").should("contain", "Bem vindo");
   });
 
-  it("Should create an account", () => {
-    cy.get('[data-test="menu-settings"]').click()
-    cy.get('[href="/contas"]').click()
-    cy.get('[data-test="nome"]').type('Teste inserção de conta Nova')
+  it("Should create a bill", () => {
+    cy.get('[data-test="menu-settings"]').click();
+    cy.get('[href="/contas"]').click();
+    cy.get('[data-test="nome"]').type("Teste inserção de conta Nova");
+    cy.get(".btn").click();
+    cy.get(".toast-message").should("contain", "Conta inserida");
+  });
+
+  it.only("Should update a bill", () => {
+    cy.get('[data-test="menu-settings"]').click();
+    cy.get('[href="/contas"]').click();
+    cy.xpath("(//i[contains(@class,'far fa-edit')])[3]").click()
+    cy.get('[data-test="nome"]')
+    .clear()
+    .type('conta de agua')
     cy.get('.btn').click()
-    cy.get('.toast-message').should('contain', 'Conta inserida')
-   
+    // cy.get('toast-message').should('contain', 'Conta atualizada')
 
   });
+
 
 
 
